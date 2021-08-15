728b8bd7b41333a255ee73c9dc96775b9efcb043
TranVanSu2001 <80629567+TranVanSu2001@users.noreply.github.com>
2021-08-15 17:31:41 +0700

    parallax scroll effect

---
+++ main.js
@@ -0,0 +1,35 @@
+const translate = document.querySelectorAll(".translate");
+const big_title = document.querySelector(".big-title");
+const header = document.querySelector("header")
+const shadow = document.querySelector(".shadow")
+const content = document.querySelector(".content")
+const section = document.querySelector("section")
+const img_container = document.querySelector(".imgContainer")
+const opacity = document.querySelectorAll(".opacity")
+const border = document.querySelector(".border")
+
+let header_height = header. offsetHeight; //trả về chiều cao của phần tử
+let section_height = section.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  let scroll = window.pageYOffset;
+  let sectionY = section.getBoundingClientRect() // trả về vị trí 4 cạnh của phần tử
+  // console.log(sectionY.top)
+
+  translate.forEach(element => {
+    let speed = element.dataset.speed;
+    element.style.transform = `translateY(${scroll * speed}px)`;
+  })
+
+  opacity.forEach(element => {
+    element.style.opacity = scroll / (sectionY.top + section_height);
+  })
+
+  big_title.style.opacity = - scroll / (header_height / 2) + 1;
+  shadow.style.height = `${scroll * 0.5 + 300}px`;
+
+  content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 -50}px)`;
+  img_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`
+
+  border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`
+})
